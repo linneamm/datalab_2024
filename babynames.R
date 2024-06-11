fa30520ff8a66aefd28497f45700dc027967d968
Linnea Minich <linneaminich@gmail.com>
2024-06-11 15:03:43 -0500

    oops, didn't save

---
+++ babynames.R
@@ -0,0 +1,31 @@
+#load library
+library(babynames)
+#Make an object called bb_names
+bb_names <- babynames
+#load more libraries
+library(ggplot2)
+library(tidyverse)
+#Create a histogram for the name Marie since 1982. Notice anything weird?
+ggplot(data = bb_names %>% filter(name=="Marie", year >= 1981))+
+  geom_col(aes(x= year,y = n))
+#Create a line plot for proportion of the name Joe, colored by sex.
+ggplot(data = bb_names %>%filter(name=="Joe"))+
+  geom_line(aes(x=year, y=prop, color = sex), alpha = .5, linewidth=2)+
+  labs(x = "Year", 
+       y = "Proportion",
+       color = "Sex",
+       title = "Proportion of the name Joe over time",
+       subtitle = "1880-2017" )
+
+max(bb_names$year) 
+min(bb_names$year)
+
+#Create a bar chart of all female names in 2002.
+ggplot(data = bb_names %>% filter(year == 2002, sex == "F", name %in% c("Madison", "Sarah", "Emma", "Emily", "Olivia")))+
+  geom_col(aes(x=name, y=n), alpha = .5, fill = "blue")
+
+the_nineties <- bb_names %>% 
+  filter(year >= 1990, year < 2000)
+
+write.csv(the_nineties)
+write_csv(the_nineties, file = "babynames_90s.csv")
